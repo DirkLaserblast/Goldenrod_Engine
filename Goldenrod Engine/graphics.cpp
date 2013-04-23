387a00d95367aba52e61c1559a849f77a549762c
Casey Scheide <cscheide@ucsc.edu>
2013-04-22 18:17:47 -0700

    Removed unnecessary graphics code

--- Goldenrod Engine/graphics.cpp
+++ Goldenrod Engine/graphics.cpp
@@ -32,10 +32,6 @@
 using namespace std;
 using namespace glm;
 
-#define MAX_PTS 40
-#define MAXFLOAT ((float)3.40282346638528860e+38) // taken from math.h
-#define MINFLOAT (-MAXFLOAT)
-
 Shader *shader = NULL;
 
 //Adds a colored vertex (consisting of three points) to the vertex, color, and normal vectors
@@ -108,9 +104,9 @@ Shape::Shape(vector<float> *vertexVector, vector<float> *colorVector, vector<flo
 //UBTextureGL texture;
 
 int WIN_WIDTH = 1280, WIN_HEIGHT = 720; //window width/height
-glm::mat4 modelView, projection, camera; //matrices for shaders
-glm::vec3 lightPos(0,0,1), viewPos(10,10,10);
-glm::mat4 modelTrans, mTrans, crTrans, csTrans, ctTrans; //Transformation matrices.
+mat4 modelView, projection, camera; //matrices for shaders
+vec3 lightPos(0,0,1), viewPos(10,10,10);
+mat4 modelTrans, mTrans, crTrans, csTrans, ctTrans; //Transformation matrices.
 float animTime = 0.0f, deltaT = 0.0001f; //variables for animation
 
 vector<Shape> shapes; //Stores all the currently rendered shapes
@@ -118,19 +114,14 @@ vector<Shape> shapes; //Stores all the currently rendered shapes
 vector<float> raw;
 vector<float> verts; //vertex array
 vector<float> norms;
-vector<float> aNorms;
-vector<float> axes;
+
 vector<float> color; //Model color array
-vector<float> aColor; //Axis and Bounding Box color array
-vector<unsigned short> indices;
-//vector<float> texs; //texture coordinates
 size_t numVerts; //number of total vertices
 size_t numIndex;
-size_t numAxes;
 
-glm::vec3 minpt;
-glm::vec3 maxpt;
-glm::vec3 centerpt;
+vec3 minpt;
+vec3 maxpt;
+vec3 centerpt;
 
 int numCoords;
 int numPolygons;
@@ -162,7 +153,7 @@ void update(float dt)
 {
     animTime += dt;
 
-    modelView = glm::rotate(modelView, glm::float_t(0.01), glm::vec3(1,1,0));
+    modelView = rotate(modelView, float_t(0.01), vec3(1,1,0));
 }
 
 //reshape function for GLUT
@@ -170,11 +161,11 @@ void reshape(int w, int h)
 {
     WIN_WIDTH = w;
     WIN_HEIGHT = h;
-    projection = glm::perspective(
-            glm::float_t(45),
-            glm::float_t(WIN_WIDTH) / glm::float_t(WIN_HEIGHT),
-            glm::float_t(0.1),
-            glm::float_t(1000.0)
+    projection = perspective(
+            float_t(45),
+            float_t(WIN_WIDTH) / float_t(WIN_HEIGHT),
+            float_t(0.1),
+            float_t(1000.0)
     );
 }
 
@@ -187,25 +178,25 @@ void display()
     //Resets all transformation matrices.
     if (r_down == true)
     {
-    	modelView = glm::mat4();
-    	mTrans = glm::mat4();
-    	crTrans = glm::mat4();
-    	csTrans = glm::mat4();
-    	ctTrans = glm::mat4();
+    	modelView = mat4();
+    	mTrans = mat4();
+    	crTrans = mat4();
+    	csTrans = mat4();
+    	ctTrans = mat4();
         r_down = false;
     }
 
     //Setup the modelview matrix
     //Mat4x4F modelCam = camera * modelView;
-    camera = glm::lookAt(glm::vec3(10,10,10), glm::vec3(0,0,0), glm::vec3(0,0,1));
+    camera = lookAt(vec3(10,10,10), vec3(0,0,0), vec3(0,0,1));
     camera = crTrans * csTrans * ctTrans * camera;
-    glm::mat4 modelCam = camera * modelView;
+    mat4 modelCam = camera * modelView;
 
     //grab the normal matrix from the modelview matrix (upper 3x3 entries of
     //modelview).
-    glm::mat3 normalMatrix(modelCam);
-    normalMatrix = glm::inverse(normalMatrix);
-    normalMatrix = glm::transpose(normalMatrix);
+    mat3 normalMatrix(modelCam);
+    normalMatrix = inverse(normalMatrix);
+    normalMatrix = transpose(normalMatrix);
 
     //Tell OpenGL which shader program we want to use. In this case, we are only
     //using one, but in general we might have many shader programs.
@@ -216,88 +207,18 @@ void display()
             shader->modelViewLoc, //handle to variable in the shader program
             1, //how many matrices we want to send
             GL_FALSE, //transpose the matrix
-            glm::value_ptr(modelCam) //a pointer to an array containing the entries for
+            value_ptr(modelCam) //a pointer to an array containing the entries for
               //the matrix
             );
     glUniformMatrix4fv(shader->projectionLoc, 1, GL_FALSE,
-            glm::value_ptr(projection));
+            value_ptr(projection));
     glUniformMatrix3fv(shader->normalMatrixLoc, 1, GL_FALSE,
-            glm::value_ptr(normalMatrix));
+            value_ptr(normalMatrix));
     glUniform1f(shader->timeLoc, animTime);
-    glUniform3fv(shader->lightPosLoc, 1, glm::value_ptr(lightPos));
-    glUniform3fv(shader->viewPosLoc, 1, glm::value_ptr(viewPos));
+    glUniform3fv(shader->lightPosLoc, 1, value_ptr(lightPos));
+    glUniform3fv(shader->viewPosLoc, 1, value_ptr(viewPos));
 
-    block = true;
-    glUniform1i(shader->lBlockLoc, block);
 
-    glBindBuffer(GL_ARRAY_BUFFER, shader->axesBuffer); //which buffer we want
-    //to use
-    glEnableVertexAttribArray(shader->vertexLoc); //enable the attribute
-    glVertexAttribPointer(
-            shader->vertexLoc, //handle to variable in shader program
-            3, //vector size (e.g. for texture coordinates this could be 2).
-            GL_FLOAT, //what type of data is (e.g. GL_FLOAT, GL_INT, etc.)
-            GL_FALSE, //normalize the data?
-            0, //stride of data (e.g. offset in bytes). Most of the time leaving
-              //this at 0 (assumes data is in one, contiguous array) is fine
-              //unless we're doing something really complex.
-            NULL //since our stride will be 0 in general, leaving this NULL is
-              //also fine in general
-            );
-
-    glBindBuffer(GL_ARRAY_BUFFER, shader->aNormalBuffer); //which buffer we want
-    //to use
-    glEnableVertexAttribArray(shader->normalLoc); //enable the attribute
-    glVertexAttribPointer(
-            shader->normalLoc, //handle to variable in shader program
-            3, //vector size (e.g. for texture coordinates this could be 2).
-            GL_FLOAT, //what type of data is (e.g. GL_FLOAT, GL_INT, etc.)
-            GL_FALSE, //normalize the data?
-            0, //stride of data (e.g. offset in bytes). Most of the time leaving
-              //this at 0 (assumes data is in one, contiguous array) is fine
-              //unless we're doing something really complex.
-            NULL //since our stride will be 0 in general, leaving this NULL is
-              //also fine in general
-            );
-
-//	glBindBuffer(GL_ARRAY_BUFFER, shader->texBuffer);
-//    glEnableVertexAttribArray(shader->texCoordLoc);
-//    glVertexAttribPointer(shader->texCoordLoc, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-
-    glBindBuffer(GL_ARRAY_BUFFER, shader->aColorBuffer);
-    glBufferData(
-            GL_ARRAY_BUFFER,
-            aColor.size() * sizeof(float),
-            aColor.data(),
-            GL_DYNAMIC_DRAW
-            );
-    glEnableVertexAttribArray(shader->colorLoc);
-    glVertexAttribPointer(shader->colorLoc, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-    //glDrawArrays(GL_LINES, 6, numAxes); //Draw bounding box
-
-    //Have to reinsert the modelView matrix, as an identity matrix,
-    //    in order to properly draw axes.
-    modelTrans = modelView;
-	modelView = glm::mat4();
-    modelCam = camera * modelView;
-    glUniformMatrix4fv(
-            shader->modelViewLoc, //handle to variable in the shader program
-            1, //how many matrices we want to send
-            GL_FALSE, //transpose the matrix
-            glm::value_ptr(modelCam) //a pointer to an array containing the entries for
-              //the matrix
-            );
-    glDrawArrays(GL_LINES, 0, 6);
-
-    modelView = modelTrans;
-    modelCam = camera * modelView;
-    glUniformMatrix4fv(
-            shader->modelViewLoc, //handle to variable in the shader program
-            1, //how many matrices we want to send
-            GL_FALSE, //transpose the matrix
-            glm::value_ptr(modelCam) //a pointer to an array containing the entries for
-              //the matrix
-            );
     block = false;
     glUniform1i(shader->lBlockLoc, block);
 
@@ -462,41 +383,41 @@ void mouseMove(int x, int y)
     upMouseXPos = downMouseXPos;
     upMouseYPos = downMouseYPos;
 
-    modelView = glm::mat4();
+    modelView = mat4();
 
     if (o_down == true)
     {
         if (x_down == true && leftbDown == true)
-            mTrans = glm::translate(mTrans, glm::vec3((static_cast<float>(mousePosDiff) / -10),0,0));
+            mTrans = translate(mTrans, vec3((static_cast<float>(mousePosDiff) / -10),0,0));
         else if (y_down == true && leftbDown == true)
-            mTrans = glm::translate(mTrans, glm::vec3(0,(static_cast<float>(mousePosDiff) / 10),0));
+            mTrans = translate(mTrans, vec3(0,(static_cast<float>(mousePosDiff) / 10),0));
         else if (z_down == true && leftbDown == true)
-            mTrans = glm::translate(mTrans, glm::vec3(0,0,(static_cast<float>(mousePosDiff) / -10)));
+            mTrans = translate(mTrans, vec3(0,0,(static_cast<float>(mousePosDiff) / -10)));
         else if (s_down == true && leftbDown == true)
-            mTrans = glm::scale(mTrans, glm::vec3(pow(1.1f, (static_cast<float>(mousePosDiff))),pow(1.1f, (static_cast<float>(mousePosDiff))),pow(1.1f, (static_cast<float>(mousePosDiff)))));
+            mTrans = scale(mTrans, vec3(pow(1.1f, (static_cast<float>(mousePosDiff))),pow(1.1f, (static_cast<float>(mousePosDiff))),pow(1.1f, (static_cast<float>(mousePosDiff)))));
         else if (x_down == true && rightbDown == true)
-            mTrans = glm::rotate(mTrans, static_cast<float>(mousePosDiff), glm::vec3(1,0,0));
+            mTrans = rotate(mTrans, static_cast<float>(mousePosDiff), vec3(1,0,0));
         else if (y_down == true && rightbDown == true)
-            mTrans = glm::rotate(mTrans, static_cast<float>(mousePosDiff), glm::vec3(0,1,0));
+            mTrans = rotate(mTrans, static_cast<float>(mousePosDiff), vec3(0,1,0));
         else if (z_down == true && rightbDown == true)
-            mTrans = glm::rotate(mTrans, static_cast<float>(mousePosDiff), glm::vec3(0,0,1));
+            mTrans = rotate(mTrans, static_cast<float>(mousePosDiff), vec3(0,0,1));
     }
     else if (c_down == true)
     {
         if (x_down == true && leftbDown == true)
-            ctTrans = glm::translate(ctTrans, glm::vec3((static_cast<float>(mousePosDiff) / -10),0,0));
+            ctTrans = translate(ctTrans, vec3((static_cast<float>(mousePosDiff) / -10),0,0));
         else if (y_down == true && leftbDown == true)
-            ctTrans = glm::translate(ctTrans, glm::vec3(0,(static_cast<float>(mousePosDiff) / 10),0));
+            ctTrans = translate(ctTrans, vec3(0,(static_cast<float>(mousePosDiff) / 10),0));
         else if (z_down == true && leftbDown == true)
-            ctTrans = glm::translate(ctTrans, glm::vec3(0,0,(static_cast<float>(mousePosDiff) / -10)));
+            ctTrans = translate(ctTrans, vec3(0,0,(static_cast<float>(mousePosDiff) / -10)));
         else if (s_down == true && leftbDown == true)
-            csTrans = glm::scale(csTrans, glm::vec3(pow(1.1f, (static_cast<float>(mouseYPosDiff))),pow(1.1f, (static_cast<float>(mouseYPosDiff))),pow(1.1f, (static_cast<float>(mouseYPosDiff)))));
+            csTrans = scale(csTrans, vec3(pow(1.1f, (static_cast<float>(mouseYPosDiff))),pow(1.1f, (static_cast<float>(mouseYPosDiff))),pow(1.1f, (static_cast<float>(mouseYPosDiff)))));
         else if (x_down == true && rightbDown == true)
-            crTrans = glm::rotate(crTrans, static_cast<float>(mousePosDiff), glm::vec3(1,0,0));
+            crTrans = rotate(crTrans, static_cast<float>(mousePosDiff), vec3(1,0,0));
         else if (y_down == true && rightbDown == true)
-            crTrans = glm::rotate(crTrans, static_cast<float>(mousePosDiff), glm::vec3(0,1,0));
+            crTrans = rotate(crTrans, static_cast<float>(mousePosDiff), vec3(0,1,0));
         else if (z_down == true && rightbDown == true)
-            crTrans = glm::rotate(crTrans, static_cast<float>(mousePosDiff), glm::vec3(0,0,1));
+            crTrans = rotate(crTrans, static_cast<float>(mousePosDiff), vec3(0,0,1));
     }
 
     modelView = modelView * mTrans;
@@ -534,86 +455,33 @@ void setupGL()
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
-    //initialize vertex and normal arrays
+	vector<vec3> testShapeVerts;
 
-	addVertex(&axes, &aColor, &aNorms, vec3(0,0,0), vec4(1,0,0,1));
-	addVertex(&axes, &aColor, &aNorms, vec3(5,0,0), vec4(1,0,0,1));
+	testShapeVerts.push_back(vec3(0,0,0));
+	testShapeVerts.push_back(vec3(1,0,0));
+	testShapeVerts.push_back(vec3(1,1,0));
+	testShapeVerts.push_back(vec3(0,1,0));
+	testShapeVerts.push_back(vec3(0,0,0));
 
-	//addVertex(&axes, &aColor, &aNorms, vec3(0,0,0), vec4(1,0,0,1));
+	addShape(testShapeVerts, vec4(1,1,0,1));
 
-    axes.push_back(0);
-    axes.push_back(0);
-    axes.push_back(0);
-    aColor.push_back(0);aColor.push_back(1);aColor.push_back(0);aColor.push_back(1);
-    aNorms.push_back(1);aNorms.push_back(1);aNorms.push_back(1);
+	testShapeVerts.clear();
 
-    axes.push_back(0);
-    axes.push_back(5);
-    axes.push_back(0);
-    aColor.push_back(0);aColor.push_back(1);aColor.push_back(0);aColor.push_back(1);
-    aNorms.push_back(1);aNorms.push_back(1);aNorms.push_back(1);
+	testShapeVerts.push_back(vec3(0,0,4));
+	testShapeVerts.push_back(vec3(1,0,4));
+	testShapeVerts.push_back(vec3(1,1,4));
+	testShapeVerts.push_back(vec3(0,1,4));
+	testShapeVerts.push_back(vec3(0,0,4));
 
-    axes.push_back(0);
-    axes.push_back(0);
-    axes.push_back(0);;
-    aColor.push_back(0);aColor.push_back(0);aColor.push_back(1);aColor.push_back(1);
-    aNorms.push_back(1);aNorms.push_back(1);aNorms.push_back(1);
+	addShape(testShapeVerts, vec4(0,1,1,1));
 
-    axes.push_back(0);
-    axes.push_back(0);
-    axes.push_back(5);
-    aColor.push_back(0);aColor.push_back(0);aColor.push_back(1);aColor.push_back(1);
-    aNorms.push_back(1);aNorms.push_back(1);aNorms.push_back(1);
+    camera = lookAt(vec3(10,10,10), vec3(0,0,0), vec3(0,0,1));
 
-    numAxes = axes.size() / 3;
-
-
-
-	//vector<vec3> testShapeVerts;
-
-	//testShapeVerts.push_back(vec3(0,0,0));
-	//testShapeVerts.push_back(vec3(1,0,0));
-	//testShapeVerts.push_back(vec3(1,1,0));
-	//testShapeVerts.push_back(vec3(0,1,0));
-	//testShapeVerts.push_back(vec3(0,0,0));
-
-	//Shape testShape = Shape(&verts, &color, &norms, testShapeVerts, vec4(1,1,1,1));
-	//shapes.push_back(testShape);
-
-	//for (int i = 0; i < (int) verts.size(); i++)
-	//{
-	//	cout << verts[i] << " ";
-	//}
-	//cout << "\n";
-
-	//testShapeVerts.clear();
-
-	//testShapeVerts.push_back(vec3(0,0,4));
-	//testShapeVerts.push_back(vec3(1,0,4));
-	//testShapeVerts.push_back(vec3(1,1,4));
-	//testShapeVerts.push_back(vec3(0,1,4));
-	//testShapeVerts.push_back(vec3(0,0,4));
-
-
-	//testShape = Shape(&verts, &color, &norms, testShapeVerts, vec4(1,1,1,1));
-	//shapes.push_back(testShape);
-
-	//for (int i = 0; i < (int) verts.size(); i++)
-	//{
-	//	cout << verts[i] << " ";
-	//}
-	//cout << "\n";
-
-
-	numVerts = verts.size() / 3;
-
-    camera = glm::lookAt(glm::vec3(10,10,10), glm::vec3(0,0,0), glm::vec3(0,0,1));
-
-    projection = glm::perspective(
-            glm::float_t(45),
-            glm::float_t(WIN_WIDTH) / glm::float_t(WIN_HEIGHT),
-            glm::float_t(0.1),
-            glm::float_t(1000.0)
+    projection = perspective(
+            float_t(45),
+            float_t(WIN_WIDTH) / float_t(WIN_HEIGHT),
+            float_t(0.1),
+            float_t(1000.0)
     );
 }
 
@@ -640,20 +508,15 @@ void setupShaders()
     shader->vertexLoc = glGetAttribLocation(shader->program, "pos");
     shader->normalLoc = glGetAttribLocation(shader->program, "norm");
     shader->colorLoc = glGetAttribLocation(shader->program, "color");
-//	shader->texCoordLoc = glGetAttribLocation(shader->program, "tex");
 
     //Create buffers for the vertex and normal attribute arrays
-    GLuint bufs[8];
-    glGenBuffers(8, bufs);
+    GLuint bufs[4];
+    glGenBuffers(4, bufs);
 
     shader->vertexBuffer = bufs[0];
     shader->normalBuffer = bufs[1];
     shader->colorBuffer = bufs[2];
-    shader->axesBuffer = bufs[3];
-    shader->aNormalBuffer = bufs[4];
-    shader->aColorBuffer = bufs[5];
-    shader->indexBuffer = bufs[6];
-//	shader->texBuffer = bufs[7];
+    shader->indexBuffer = bufs[3];
 
     //This is where we pass the vertex/normal data to the GPU.
     //In general, the procedure for working with buffers is:
@@ -691,52 +554,13 @@ void setupShaders()
             GL_DYNAMIC_DRAW
             );
 
-    glBindBuffer(GL_ARRAY_BUFFER, shader->axesBuffer);
-    glBufferData(
-            GL_ARRAY_BUFFER, //what kind of buffer (an array)
-            axes.size() * sizeof(float), //size of the buffer in bytes
-            axes.data(), //pointer to data we want to fill the buffer with
-            GL_DYNAMIC_DRAW //how we intend to use the buffer
-            );
-
-    glBindBuffer(GL_ARRAY_BUFFER, shader->aNormalBuffer);
-    glBufferData(
-            GL_ARRAY_BUFFER, //what kind of buffer (an array)
-            aNorms.size() * sizeof(float), //size of the buffer in bytes
-            aNorms.data(), //pointer to data we want to fill the buffer with
-            GL_STATIC_DRAW //how we intend to use the buffer
-            );
-
-    glBindBuffer(GL_ARRAY_BUFFER, shader->aColorBuffer);
-    glBufferData(
-            GL_ARRAY_BUFFER,
-            aColor.size() * sizeof(float),
-            aColor.data(),
-            GL_DYNAMIC_DRAW
-            );
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, shader->indexBuffer);
-    glBufferData(
-            GL_ELEMENT_ARRAY_BUFFER,
-            indices.size() * sizeof(GLushort),
-            indices.data(),
-            GL_STATIC_DRAW
-            );
-
-	//glBindBuffer(GL_ARRAY_BUFFER, shader->texBuffer);
- //   glBufferData(
- //           GL_ARRAY_BUFFER,
- //           texs.size() * sizeof(float),
- //           texs.data(),
- //           GL_STATIC_DRAW
- //           );
 }
 
 void findNorms()
 {
-	glm::vec3 nVec1;
-	glm::vec3 nVec2;
-	glm::vec3 rVec;
+	vec3 nVec1;
+	vec3 nVec2;
+	vec3 rVec;
 	for (int niter = 0; niter < (int) verts.size(); niter+=9)
 	{
 		nVec1.x = verts[niter] - verts[niter + 3];
@@ -747,7 +571,7 @@ void findNorms()
 		nVec2.y = verts[niter + 7] - verts[niter + 4];
 		nVec2.z = verts[niter + 8] - verts[niter + 5];
 
-		rVec = glm::cross(nVec2, nVec1);
+		rVec = cross(nVec2, nVec1);
 		norms.push_back(rVec.x);
 		norms.push_back(rVec.y);
 		norms.push_back(rVec.z);
